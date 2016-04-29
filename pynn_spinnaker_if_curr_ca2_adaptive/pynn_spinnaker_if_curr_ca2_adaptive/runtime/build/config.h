15f459f76bbb3c566155485a6c65886b44019f78
neworderofjamie <james.knight@manchester.ac.uk>
2016-04-29 11:40:55 +0100

    * Select intrinsic plasticity stub for extra models * Also fixed gitignore mess

--- pynn_spinnaker_if_curr_ca2_adaptive/pynn_spinnaker_if_curr_ca2_adaptive/runtime/build/config.h
+++ pynn_spinnaker_if_curr_ca2_adaptive/pynn_spinnaker_if_curr_ca2_adaptive/runtime/build/config.h
@@ -3,16 +3,16 @@
 // Model includes
 #include "neuron_processor/input_buffer.h"
 #include "neuron_processor/intrinsic_plasticity_models/stub.h"
-#include "neuron_processor/neuron_models/if_curr.h"
-#include "../dual_exp.h"
+#include "neuron_processor/synapse_models/exp.h"
+#include "../ca2_adaptive.h"
 
 namespace NeuronProcessor
 {
 //-----------------------------------------------------------------------------
 // Typedefines
 //-----------------------------------------------------------------------------
-typedef NeuronModels::IFCurr Neuron;
-typedef ExtraModels::DualExp Synapse;
+typedef ExtraModels::CA2Adaptive Neuron;
+typedef SynapseModels::Exp Synapse;
 typedef IntrinsicPlasticityModels::Stub IntrinsicPlasticity;
 
 typedef InputBufferBase<uint32_t> InputBuffer;
