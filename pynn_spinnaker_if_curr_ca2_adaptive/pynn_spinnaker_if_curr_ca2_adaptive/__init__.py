07894375c8e22d1c3c804dd6cf9a41c46211ded1
neworderofjamie <james.knight@manchester.ac.uk>
2016-05-05 16:11:56 +0100

    Export innards of module

--- pynn_spinnaker_if_curr_ca2_adaptive/pynn_spinnaker_if_curr_ca2_adaptive/__init__.py
+++ pynn_spinnaker_if_curr_ca2_adaptive/pynn_spinnaker_if_curr_ca2_adaptive/__init__.py
@@ -1,2 +1,7 @@
 # Import classes
 from if_curr_ca2_adaptive import IF_curr_ca2_adaptive_exp
+
+# Import globals
+from if_curr_ca2_adaptive import (if_curr_ca2_adaptive_neuron_translations,
+                                  if_curr_ca2_adaptive_neuron_immutable_param_map,
+                                  if_curr_ca2_adaptive_neuron_mutable_param_map)
--- pynn_spinnaker_if_curr_dual_exp/pynn_spinnaker_if_curr_dual_exp/__init__.py
+++ pynn_spinnaker_if_curr_dual_exp/pynn_spinnaker_if_curr_dual_exp/__init__.py
@@ -1,2 +1,7 @@
 # Import classes
 from if_curr_dual_exp import IF_curr_dual_exp
+
+# Import globals
+from if_curr_dual_exp import (dual_exp_synapse_translations,
+                              dual_exp_synapse_immutable_param_map,
+                              dual_exp_synapse_curr_mutable_param_map)
